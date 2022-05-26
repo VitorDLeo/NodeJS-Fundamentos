aca882ce4363d3b9b28901751415b1d6d5f18ebe
VitorDLeo <vitordileo003@gmail.com>
2022-05-26 16:13:50 -0300

    Add nodemon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const { send } = require("express/lib/response");
 
 const app = express();
 
 // Porta
 app.listen(3333);
+
+app.get("/", (request, response) => {
+    return response.json({ message: "Hello World - NodeJS"});
+});
