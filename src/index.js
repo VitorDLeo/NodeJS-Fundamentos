622767253c8779d9d2a5dba7586747625a0cb54e
VitorDLeo <vitordileo003@gmail.com>
2022-05-26 19:40:07 -0300

    Add methods GET, POST, PUT, DELETE and Patch

--- src/index.js
+++ src/index.js
@@ -7,19 +7,30 @@ const app = express();
 // Porta
 app.listen(3333);
 
+/*  
+    Tipos de parametros
+
+    Route Parameters - Usados para identificar um recurso - Excluir/editar/Buscar
+    Query Parameters - Usados para paginacao / filtros
+    Body Parameters - Os objetos de insercao/alteracao (JSON)
+*/
+
 app.get("/courses", (request, response) => {
     return response.json(["Curso1", "Curso2", "Curso3"]);
 });
 
 app.post("/courses", (request, response) => {
+    const body = 
     return response.json(["Curso1", "Curso2", "Curso3", "Curso4"]);
 });
 
 app.put("/courses/:id", (request, response) => {
+    const { id } = request.params;
+    console.log(id);
     return response.json(["Curso6", "Curso2", "Curso3", "Curso4"]);
 });
 
-app.path("/courses/:id", (request, response) =>{
+app.patch("/courses/:id", (request, response) =>{
     return response.json(["Curso6", "Curso7", "Curso3", "Curso4"]);
 });
 
