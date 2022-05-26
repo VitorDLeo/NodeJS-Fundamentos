74b92f2328adaae1997635e83a9d0eb3e9c9810b
VitorDLeo <vitordileo003@gmail.com>
2022-05-26 16:29:09 -0300

    Add methods GET, POST, PUT, DELETE and Patch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { request } = require("express");
 const express = require("express");
 const { send } = require("express/lib/response");
 
@@ -6,6 +7,22 @@ const app = express();
 // Porta
 app.listen(3333);
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World - NodeJS"});
+app.get("/courses", (request, response) => {
+    return response.json(["Curso1", "Curso2", "Curso3"]);
 });
+
+app.post("/courses", (request, response) => {
+    return response.json(["Curso1", "Curso2", "Curso3", "Curso4"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+    return response.json(["Curso6", "Curso2", "Curso3", "Curso4"]);
+});
+
+app.path("/courses/:id", (request, response) =>{
+    return response.json(["Curso6", "Curso7", "Curso3", "Curso4"]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json(["Curso6", "Curso7", "Curso4"]);
+})
